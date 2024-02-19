6ae9ce6f08b1cadf9566ac73285022fcaffc6e3d
Patrick Annan <you@example.compatrickannan@hotmail.com>
2024-02-19 15:01:09 -0500

    feb 19 push

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,17 +6,23 @@ const handleErrors = (err) => {
   console.log(err.message, err.code);
   let errors = {email: '', password: ''}
 
+  // incorrect email
   if (err.message === 'Incorrect Email') {
     errors.email = 'That email is not registered';
   }
+
+  //incorrect password
 if (err.message === 'Incorrect Password') {
   errors.password = 'That password is incorrect';
 }
 
+  // duplicate error code
   if(err.code == 11000){
     errors.email = 'This email is already in use'
     return errors
   }
+
+  // validation errors
   if(err.message.includes('user validation failed')){
     Object.values(err.errors).forEach(({properties}) => {
       errors[properties.path] = properties.message

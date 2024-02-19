92d634b02e25496f14fc0d4852d3edcf1799a1c6
Patrick Annan <you@example.compatrickannan@hotmail.com>
2024-02-19 14:13:49 -0500

    first push

--- controllers/authController.js
+++ controllers/authController.js
@@ -64,7 +64,7 @@ module.exports.signup_get = (req, res) => {
       const token = createToken(user._id)
       res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
       res.status(200).json({ user: user._id })
-    }
+    } 
     catch(err){
       const errors = handleErrors(err)
       res.status(400).json({ errors })
